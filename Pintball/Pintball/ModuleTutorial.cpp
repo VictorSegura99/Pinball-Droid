126bd506bb57be83981a200d406182642442da0e
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-09-16 21:13:08 +0200

    VSDVS

--- Pintball/Pintball/ModuleTutorial.cpp
+++ Pintball/Pintball/ModuleTutorial.cpp
@@ -56,7 +56,7 @@ update_status ModuleTutorial::Update()
 	SDL_RenderClear(App->render->renderer);
 	anim = &Tutorial;
 	App->render->Blit(tuto, 5, 68, &(anim->GetCurrentFrame()));
-	if ((App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_DOWN && App->fade->IsFading() == false))
+	if (((App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_M] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_Z] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_UP] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_DOWN] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_LEFT] == KEY_DOWN || App->input->keyboard[SDL_SCANCODE_RIGHT] == KEY_DOWN) && App->fade->IsFading() == false))
 	{
 		App->fade->FadeToBlack(this, (Module*)App->pintball, 0.0f);
 	}
