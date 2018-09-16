cb74f8a82833613e3accabd1cd1bd24a9dc2867d
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-09-16 17:45:18 +0200

    CRUCECITA TANCAMENT DONE OMG

--- Pintball/Pintball/ModuleInput.cpp
+++ Pintball/Pintball/ModuleInput.cpp
@@ -33,8 +33,8 @@ update_status ModuleInput::PreUpdate()
 	SDL_PumpEvents();
 
 	keyboard = SDL_GetKeyboardState(NULL);
-
-	if (keyboard[SDL_SCANCODE_ESCAPE])
+	SDL_PollEvent(&Events);
+	if (keyboard[SDL_SCANCODE_ESCAPE] || Events.type == SDL_QUIT)
 		return update_status::UPDATE_STOP;
 
 	return update_status::UPDATE_CONTINUE;
--- Pintball/Pintball/ModuleRender.cpp
+++ Pintball/Pintball/ModuleRender.cpp
@@ -49,15 +49,7 @@ update_status ModuleRender::PreUpdate()
 
 update_status ModuleRender::Update()
 {
-	int speed = 3;
 
-	if (App->input->keyboard[SDL_SCANCODE_UP] == 1)
-		camera.y += speed;
-
-	if (App->input->keyboard[SDL_SCANCODE_DOWN] == 1)
-		camera.y -= speed;
-
-	// TODO 1: Make the camera move left and right
 
 	return update_status::UPDATE_CONTINUE;
 }
